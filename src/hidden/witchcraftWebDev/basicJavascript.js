e4763208b4b0e35cd06641476ea144f41b0ddb36
herdofsheep <herdofsheep@users.noreply.github.com>
2021-01-02 19:54:20 +0000

    working pretty nice portfolio

--- src/hidden/witchcraftWebDev/basicJavascript.js
+++ src/hidden/witchcraftWebDev/basicJavascript.js
@@ -8,30 +8,57 @@ init()
 function init(){
 
     translateDistance = 100;
-    translateOffset = 4;
+    translateOffset = 0;
 
+    document.addEventListener('keydown', function(e) {
+        switch (e.key) {
+            case "ArrowLeft":
+                leftClick()
+                break;
+            case "ArrowRight":
+                rightClick()
+                break;
+        }
+    });
     images = document.getElementsByClassName('img');
     focusPos = Array.prototype.indexOf.call(images, images['focus']);
 
-    positionImages();
+    update();
 
 }
 
-function positionImages(){
+function keyPress(){
+
+}
+
+function update(){
     var translateValue = focusPos*translateDistance-translateOffset;
     var translateBy = -1 * translateValue;
+
+    if (focusPos == images.length-1){
+        document.getElementById('arrow-right').style.visibility = 'hidden';
+    }
+    if (focusPos == 0){
+        document.getElementById('arrow-left').style.visibility = 'hidden';
+    }
 
     for(var i=0; i<images.length; i++){
         images[i].style.transform = "translateX(" + translateBy + "%) scale(" + 0.8 + ")"
     }
+
+    images[focusPos].style.transform = "translateX(" + translateBy + "%) scale(" + 1 + ")"
+
+    updateText();
+}
+
+function updateText(){
+    var infoText = document.getElementById('info');
+    infoText.innerHTML = images[focusPos].firstElementChild.alt + " / 32"
 }
 
 function leftClick(){
 
     document.getElementById('arrow-right').style.visibility = 'visible';
-    if (focusPos == 1){
-        document.getElementById('arrow-left').style.visibility = 'hidden';
-    }
 
     if (focusPos > 0){
 
@@ -41,7 +68,8 @@ function leftClick(){
         images[focusPos].id = 'focus';
         images[focusPos].style.zIndex = '10';
 
-        positionImages();
+        update();
+        updateText();
 
     }
 
@@ -50,9 +78,6 @@ function leftClick(){
 function rightClick(){
 
     document.getElementById('arrow-left').style.visibility = 'visible';
-    if (focusPos == images.length-2){
-        document.getElementById('arrow-right').style.visibility = 'hidden';
-    }
 
     if (focusPos < images.length-1){
 
@@ -62,7 +87,8 @@ function rightClick(){
         images[focusPos].id = 'focus';
         images[focusPos].style.zIndex = '10';
 
-        positionImages();
+        update();
+        updateText();
 
     }
 
