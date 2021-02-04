cb0eeac1e602443bb0963d2e5f8c065ae05a4a4d
herdofsheep <herdofsheep@users.noreply.github.com>
2021-02-04 21:02:51 +0000

    working on the slider

--- src/js/art.js
+++ src/js/art.js
@@ -5,9 +5,15 @@ var images, focusPos;
 var selection, contentOptions;
 var links;
 
+var scrollMin, scrollMax;
+
 init()
 
 function init(){
+
+    var scrollDiv = document.getElementById('artMain')
+    scrollDiv.scrollLeft = 0;
+    scrollDiv.addEventListener("scroll", handleScroll)
 
     contentOptions = document.getElementsByClassName('contentOption');
     selection = Array.prototype.indexOf.call(contentOptions, contentOptions['default']);
@@ -18,13 +24,19 @@ function init(){
         links[i].addEventListener('click',linkClick);
     }
 
-    update();
+    scrollMin = document.getElementById('scroller').firstElementChild.getAttribute('min')
+    scrollMax = document.getElementById('scroller').firstElementChild.getAttribute('max')
 
 }
 
+function handleScroll(){
+    var scrollPos = document.getElementById('artMain').scrollLeft;
+    var width = document.getElementById('artMain').clientWidth;
 
-function update(){
+    var value = scrollPos*scrollMax/width;
 
+    document.getElementById('scroller').firstElementChild.setAttribute('value',value)
+    document.getElementById('scroller').firstElementChild.value = value
 }
 
 function updateText(){
@@ -34,6 +46,8 @@ function updateText(){
 
 function linkClick(e){
     var link = e.target.id;
+
+    document.getElementById('artMain').scrollLeft = 0;
 
     for(var i=0; i<contentOptions.length; i++){
         contentOptions[i].style.visibility = 'hidden'
