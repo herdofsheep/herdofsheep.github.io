a649764729e609f80ad4f6bbb33933fde91529dc
herdofsheep <herdofsheep@users.noreply.github.com>
2021-02-05 13:39:40 +0000

    building contact page

---
+++ src/js/contact.js
@@ -0,0 +1,71 @@
+
+var email = document.getElementById("email");
+email.addEventListener("click", emailClipboard);
+email.value = email.innerText;
+
+window.addEventListener("DOMContentLoaded", function() {
+        
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+        form.reset();
+        button.style = "display: none ";
+        status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+        } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
+
+function emailClipboard(){
+
+    // /* Select the text field */
+    // email.select();
+    // email.setSelectionRange(0, 99999); /* For mobile devices */
+    
+    // /* Copy the text inside the text field */
+    // document.execCommand("copy");
+
+    var range = document.createRange();
+    range.selectNode(document.getElementById("email"));
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();// to deselect
+
+    
+    /* Alert the copied text */
+    alert("Copied to clipboard " + email.value);
+}
