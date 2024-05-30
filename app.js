687b4eb8a8adfa774fe93a9b8614aadf17dcd0e3
Megan Bates <herdofsheep@gmail.com>
2024-05-30 17:41:22 +0100

    trying to fix pathing

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/assets', express.static(path.join(__dirname, '/assets')));
+app.use('/src', express.static(path.join(__dirname, '/src')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules/three/build/')));
+// app.use('/build', express.static(path.join(__dirname, '/node_modules')));
+// app.use('/three_addon', express.static(path.join(__dirname, '/node_modules')));
+app.use('/jsm', express.static(path.join(__dirname, '/node_modules/three/examples/jsm')));
+
+app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'))
