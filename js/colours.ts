f9cb361347fe63ec2d51a77a7c72705cd638d5cc
Megan Bates <herdofsheep@gmail.com>
2024-08-24 18:25:38 +0100

    working on index2d

---
+++ js/colours.ts
@@ -0,0 +1,9 @@
+const colours = {
+    very_dark_grey: 0x141414,
+    white: 0xffffff,
+    dark_grey: 0x202020,
+    light_pink: 0xFFC0CB,
+    bright_pink: 0xfb8da0,
+  };
+  
+export default colours;
