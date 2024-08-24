4c110069b78d682111ff94fb6f47daf894bd977f
Megan Bates <herdofsheep@gmail.com>
2024-08-24 23:57:06 +0100

    threedmodel starting to work

--- js/three_d_model.ts
+++ js/three_d_model.ts
@@ -1,4 +1,4 @@
-import { LitElement } from "lit";
+import { html } from "lit";
 import * as THREE from "three";
 import _ from "lodash";
 
@@ -10,11 +10,13 @@ class ThreeDModel extends ThreeBase {
 
   white: THREE.Color;
   light_pink: THREE.Color;
-  camera: THREE.OrthographicCamera;
+  dark_grey: THREE.Color;
+
+  // camera: THREE.OrthographicCamera;
   model: THREE.Group;
   width: number;
   height: number;
-  mesh: THREE.InstancedMesh;
+  mesh: THREE.Mesh;
 
   constructor() {
     super();
@@ -22,6 +24,16 @@ class ThreeDModel extends ThreeBase {
 
     this.white = new THREE.Color().setHex(colours.white);
     this.light_pink = new THREE.Color().setHex(colours.light_pink);
+    this.dark_grey = new THREE.Color().setHex(colours.dark_grey);
+
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+  }
+
+  render(){
+    return html`
+    <div id="container" ></div>
+    `;
   }
 
   async firstUpdated() {
@@ -31,36 +43,32 @@ class ThreeDModel extends ThreeBase {
     const geometry = await this.getModel("/assets/models/gltf/questionmark.glb")
     const material = new THREE.MeshPhongMaterial({ color: this.white, flatShading: true, shininess: 0 });
 
-    this.mesh = new THREE.InstancedMesh(geometry, material, 20);
+    this.mesh = new THREE.Mesh(geometry, material);
     this.setupLights();
     this.setupScene();
+    this.startAnimation();
   }
 
   setupLights() {
-    this.scene.background = new THREE.Color(this.light_pink);
+    this.scene.background = new THREE.Color(this.dark_grey);
 
-    this.camera = new THREE.OrthographicCamera(
-      this.width / -2,
-      this.width / 2,
-      this.height / 2,
-      this.height / -2,
-      -100,
-      1000,
-    );
-
-    var light = new THREE.HemisphereLight(0x470b16, 100, 10);
-    light.position.set(0, 0, 10);
-    this.scene.add(light);
-
+    this.camera = new THREE.OrthographicCamera( this.width / - 2, this.width / 2, this.height / 2, this.height / - 2, 1, 1000 );
     this.scene.add( this.mesh );
+    this.camera.position.set(0, 0, 0);
+    this.camera.lookAt(this.mesh.position);
+
+    const light_one = new THREE.SpotLight( this.white, 1.5 );
+    light_one.position.set( 0, 500, 20000 );
+    this.scene.add(light_one);
+
+    const light_two = new THREE.HemisphereLight(this.white, 0x888888, 3);
+    light_two.position.set(0, 1, 0);
+    this.scene.add(light_two);
   }
 
   setupScene() {
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.width = 100;
-    this.height = 100;
     this.renderer.setSize(this.width, this.height);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.container?.appendChild( this.renderer.domElement );
     this.renderer.setAnimationLoop(this.startAnimation);
 
